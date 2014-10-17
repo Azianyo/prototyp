dc385c040eadd59832cd5d5db213c1cae788fd8c
Azianyo <sfrustrowanaherbatamietowa@gmail.com>
2014-10-18 00:49:18 +0200

    Added display of patients information & edited display of photos

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,8 +1,5 @@
 class HomesController < ApplicationController
   def index
-    @photo_names = Dir.glob(File.join(Rails.root,"app/assets/images/photos/*.{jpg,jpeg,png}"))
-    @photo_names = @photo_names.map do |name|
-      name.gsub(File.join(Rails.root,"app/assets/images/photos/"), '')
-    end
+    @photos = Photo.all
   end
 end
