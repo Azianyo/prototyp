9ddbb4f6ef3578b196392bcb7ef712d1062a19ad
Azianyo <sfrustrowanaherbatamietowa@gmail.com>
2014-10-19 18:45:07 +0200

    Added browsing through questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,11 +2,13 @@ class QuestionsController < ApplicationController
   def index
     @photo = Photo.find(params[:photo_id])
     @suite = Suite.find(params[:suite_id])
-    @question = Question.where(:suite_id => @suite.id).order(:id).first
+    @question = @suite.questions.order(:id).first
     redirect_to photo_suite_question_path(@photo, @suite, @question)
   end
+
   def show
+    @photo = Photo.find(params[:photo_id])
+    @suite = Suite.find(params[:suite_id])
     @question = Question.find(params[:id])
   end
-
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,12 @@
 class Question < ActiveRecord::Base
   belongs_to :suite
   has_one :answer
+
+   def next
+    suite.questions.where('id > ?', id).order(:id => :asc).first
+  end
+
+  def prev
+    suite.questions.where('id < ?', id).order(:id => :desc).first
+  end
 end
