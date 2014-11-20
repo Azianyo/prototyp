a1d090ab49b4f74af045e659ed66126dc928c683
Azianyo <sfrustrowanaherbatamietowa@gmail.com>
2014-11-20 07:01:48 +0100

    Added users percentage count

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
   def show
     @photos_done = []
+    @users_all_points = 0
     @users_points = UsersTestStat.where(:user_id => current_user.id).pluck(:points).reduce(:+)
     UsersTestStat.all.each do |usersstat|
+      @users_all_points += (usersstat.points*100)/usersstat.percent
       @photos_done << Photo.find(usersstat.photo_id)
     end
+    @users_percentage = (@users_points/@users_all_points)*100
     @photos_done = @photos_done.uniq
   end
 end
