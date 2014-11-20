34bc7dd06df4a32a190849d64194f18b897774d2
Azianyo <sfrustrowanaherbatamietowa@gmail.com>
2014-11-20 05:05:48 +0100

    Made percentage div's width dynamic

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,7 @@
 class DashboardController < ApplicationController
   def index
     @photos = Photo.all
+    @point_check_percent = 0
   end
 
 end
--- app/models/users_test_stat.rb
+++ app/models/users_test_stat.rb
@@ -2,4 +2,12 @@ class UsersTestStat < ActiveRecord::Base
   belongs_to :user
   belongs_to :suite
   belongs_to :photo
+
+  def self.percentage(suite_name, user_id, photo_id)
+    if UsersTestStat.where(:user_id => user_id, :photo_id => photo_id, :suite_id => Suite.find_by(:name => suite_name).id).order(:times_done).last.nil? then
+      0
+    else
+      UsersTestStat.where(:user_id => user_id, :photo_id => photo_id, :suite_id => Suite.find_by(:name => suite_name).id).order(:times_done).last.percent
+    end
+  end
 end
