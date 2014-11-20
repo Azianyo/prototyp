87719cf8850404800baec140aea7a53ca5bfa4fa
Azianyo <sfrustrowanaherbatamietowa@gmail.com>
2014-11-20 06:24:17 +0100

    Adapted point count & on user's profile to recent changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,9 @@
 class UsersController < ApplicationController
   def show
+    @photos_done = []
     @users_points = UsersTestStat.where(:user_id => current_user.id).pluck(:points).reduce(:+)
-    @answer_ids_done = Reply.where(:user_id => current_user.id).order("updated_at desc").pluck(:answer_id)
-    @photos_done = Array.new
-    @answer_ids_done.each do |answer_id|
-      @photos_done << Photo.find(Answer.find(answer_id).photo_id)
+    UsersTestStat.all.each do |usersstat|
+      @photos_done << Photo.find(usersstat.photo_id)
     end
     @photos_done = @photos_done.uniq
   end
