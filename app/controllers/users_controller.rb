932feef3ae12f20bd7c09318bf3f0c64e1c83902
Azianyo <sfrustrowanaherbatamietowa@gmail.com>
2014-11-24 15:39:23 +0100

    Changed users profile to work for users that are not logged in, added links to forgot password? & password change + added photo in answering forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,16 @@
 class UsersController < ApplicationController
   def show
+    if current_user.present? then
+      @user = current_user
+    else
+      @user = User.find(params[:id])
+    end
     @photos_done = []
     @users_all_points = 0
-    @users_points = UsersTestStat.where(:user_id => current_user.id).pluck(:points).reduce(:+)
-    UsersTestStat.all.each do |usersstat|
+    @users_stats = UsersTestStat.where(:user_id => @user.id)
+
+    @users_points = @users_stats.pluck(:points).reduce(:+)
+    @users_stats.each do |usersstat|
       @users_all_points += (usersstat.points*100)/usersstat.percent
       @photos_done << Photo.find(usersstat.photo_id)
     end
