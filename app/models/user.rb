db00f724b98f6c8cf43b0eab8336de847592b5d5
Azianyo <sfrustrowanaherbatamietowa@gmail.com>
2014-11-27 16:00:41 +0100

    Fixed user authentication & photos are now only clickable for logged in users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [ :name ]
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :users_test_stats
   validates_presence_of :name, :email, :password
 end
