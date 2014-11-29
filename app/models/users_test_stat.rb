72c9bb6b74847803cbeae38cc2c5322ac869ce1d
Azianyo <sfrustrowanaherbatamietowa@gmail.com>
2014-11-29 19:13:39 +0100

    Added description for percentage strips

--- app/models/users_test_stat.rb
+++ app/models/users_test_stat.rb
@@ -5,9 +5,11 @@ class UsersTestStat < ActiveRecord::Base
 
   def self.percentage(suite_name, user_id, photo_id)
     if UsersTestStat.find_by(:user_id => user_id, :photo_id => photo_id, :suite_id => Suite.find_by(:name => suite_name).id).nil? then
-      0
+      "1px"
+    elsif UsersTestStat.find_by(:user_id => user_id, :photo_id => photo_id, :suite_id => Suite.find_by(:name => suite_name).id).percent == 0 then
+      "1px"
     else
-      UsersTestStat.find_by(:user_id => user_id, :photo_id => photo_id, :suite_id => Suite.find_by(:name => suite_name).id).percent
+      "#{UsersTestStat.find_by(:user_id => user_id, :photo_id => photo_id, :suite_id => Suite.find_by(:name => suite_name).id).percent}%"
     end
   end
 end
