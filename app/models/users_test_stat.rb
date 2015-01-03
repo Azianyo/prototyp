dacacae0bfd21c332e37ba557fb07f1bb5710a29
Azianyo <sfrustrowanaherbatamietowa@gmail.com>
2015-01-03 03:50:36 +0100

    Added buttons to suite-index view, fixed line visibility, changed diff buttons

--- app/models/users_test_stat.rb
+++ app/models/users_test_stat.rb
@@ -5,9 +5,9 @@ class UsersTestStat < ActiveRecord::Base
 
   def self.percentage(suite_name, user_id, photo_id)
     if UsersTestStat.find_by(:user_id => user_id, :photo_id => photo_id, :suite_id => Suite.find_by(:name => suite_name).id).nil? then
-      "1px"
+      "0px"
     elsif UsersTestStat.find_by(:user_id => user_id, :photo_id => photo_id, :suite_id => Suite.find_by(:name => suite_name).id).percent == 0 then
-      "1px"
+      "0px"
     else
       "#{UsersTestStat.find_by(:user_id => user_id, :photo_id => photo_id, :suite_id => Suite.find_by(:name => suite_name).id).percent}%"
     end
